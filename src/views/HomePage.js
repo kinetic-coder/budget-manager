ccca76a15415e27d1a5aca4350a1b0cd5b0713ab
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2020-10-26 12:00:49 +0000

    Added basic navigation on home page.

---
+++ src/views/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import NavigationBar from "../components/NavigationBar";
+
+const HomePage = () => (
+  <>
+    <NavigationBar />
+    <h1>Home Page</h1>
+  </>
+);
+
+export default HomePage;
